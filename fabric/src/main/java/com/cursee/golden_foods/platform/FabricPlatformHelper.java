82693392c0184611ec55f6c69e611981d9862347
jason13official <jason13official@gmail.com>
2024-10-28 18:30:03 -0400

    refactor

---
+++ fabric/src/main/java/com/cursee/golden_foods/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.cursee.golden_foods.platform;
+
+import com.cursee.golden_foods.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
