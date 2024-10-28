6e0ebe805667ce235542d46c9fa3dc5472888718
jason13official <jason13official@gmail.com>
2024-10-28 18:38:22 -0400

    fix: deprecate EnchatedBowlFoodItem

--- common/src/main/java/com/cursee/golden_foods/core/item/custom/EnchantedBowlFoodItem.java
+++ common/src/main/java/com/cursee/golden_foods/core/item/custom/EnchantedBowlFoodItem.java
@@ -1,9 +1,12 @@
 package com.cursee.golden_foods.core.item.custom;
 
-import net.minecraft.world.item.BowlFoodItem;
+// import net.minecraft.world.item.BowlFoodItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-public class EnchantedBowlFoodItem extends BowlFoodItem {
+/** Food Properties handle determining whether the item is a bowl. */
+@Deprecated(forRemoval = true, since = "1.21.1")
+public class EnchantedBowlFoodItem extends Item { // extends BowlFoodItem {
 
     public EnchantedBowlFoodItem(Properties properties) {
         super(properties);
